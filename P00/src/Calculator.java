629280e600206b79fa907ed832c5c13a61f4d86a
22044961 <22044961@LAPTOP-P16QDQMJ>
2023-05-26 11:45:42 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,23 @@
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int divide(int a, int b) {
+		if (b == 0) { 
+			return 0;
+		}else {
+			return a / b;
+		}
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+
+}
